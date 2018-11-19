551e0c2d168322015006673e5efc0f69eafce709
nanlihao <nanlihao@gmail.com>
2018-11-19 18:32:54 +1100

    sync

--- hpc_scripts/test.sh
+++ hpc_scripts/test.sh
@@ -7,4 +7,4 @@
 cd "/project/RDS-FEI-NLH-RW/work/pprli/"
 module load cuda/9.1.85 openmpi-gcc/3.0.0-cuda python/3.6.5
 source pprli_env/bin/activate
-python src/models/aeganf.py --gpu 0 train --debug --no-test
+python src/models/aeganf.py --gpu 0 train --debug --no-test --epoch 100
