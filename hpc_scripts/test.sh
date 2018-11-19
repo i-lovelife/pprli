95418a42555a24446fc70f4830277bb472d77774
nanlihao <nanlihao@gmail.com>
2018-11-19 18:33:26 +1100

    sync

--- hpc_scripts/test.sh
+++ hpc_scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PBS -P NLH                   # your project name
+#PBS -P NLH
 #PBS -q alloc-dt
-#PBS -l select=1:ncpus=8:ngpus=1:mpiprocs=8:mem=45gb  # select one chunk with 1 core and 4 GB memory
-#PBS -l walltime=10:00:00         # actual time your job will run for
+#PBS -l select=1:ncpus=8:ngpus=1:mpiprocs=8:mem=45gb
+#PBS -l walltime=10:00:00
  
 cd "/project/RDS-FEI-NLH-RW/work/pprli/"
 module load cuda/9.1.85 openmpi-gcc/3.0.0-cuda python/3.6.5
 source pprli_env/bin/activate
-python src/models/aeganf.py --gpu 0 train --debug --no-test
+python src/models/aeganf.py --gpu 0 train --debug --no-test --epoch 100
