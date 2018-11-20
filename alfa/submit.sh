11d906bc2a6fe401b6a8856ad0e4359132326c20
nanlihao <nanlihao@gmail.com>
2018-11-20 19:43:45 +1100

    experiment

--- alfa/submit.sh
+++ alfa/submit.sh
@@ -2,4 +2,4 @@
 git add --all
 git commit -m 'experiment'
 git push
-ssh lnan6257@hpc.sydney.edu.au "cd /project/RDS-FEI-NLH-RW/work/pprli; git pull; qsub $1 2>&1 "
+ssh lnan6257@hpc.sydney.edu.au "cd /project/RDS-FEI-NLH-RW/work/pprli; git pull; cd hpc_scripts/; qsub $1 2>&1 "
